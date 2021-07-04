acad92112ade1fe2ea55c1795c37dda00be083cc
Antonio dos Santos Netto <92.netto@gmail.com>
2021-07-04 16:28:13 -0300

    phporientado início dos trabalhos

---
+++ app/Libraries/Rota.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+class Rota{ 
+   
+   
+   private $controlador = 'paginas';
+   
+   
+   
+    public function __construct()
+    {
+        $url = $this->url() ? $this->url() : [0];
+        if(file_exists('../app/Controlers/'.ucwords($url[0]).'.php')):
+            $this->controlador = ucwords($url[0]);
+            unset($url[0]);
+        endif;
+        require_once '../app/Controllers/'.$this->controlador.'.php';
+        $this->controlador = new $this->controlador;
+        var_dump($this);
+        
+    }
+
+    private function url(){ 
+        $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
+        if(isset($url)){
+            $url = trim(rtrim($url, '/'));
+            $url = explode('/', $url);
+            
+            return $url;
+
+        }
+    }
+}
